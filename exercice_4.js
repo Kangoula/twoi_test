4a8af37a98733e0735a3c59893ce7f11f94fc010
Guillaume Denis <denisgme@gmail.com>
2018-09-03 21:16:30 +0200

    finish exercice_4

--- exercice_4.js
+++ exercice_4.js
@@ -14,5 +14,37 @@
 // You have to list your download folder (or any other folder) using child process
 // then write them in a basic text file
 //
-
 // good luck !
+
+const { spawn } = require('child_process');
+const fs = require('fs');
+
+const writeToFile =  data => {
+  fs.writeFile('./list', data, (err, data) => {
+    if(err){
+      console.error('an error occured during write', err);
+    } else {
+      console.log('file saved');
+    }
+  });
+};
+
+const listDirectory = path => {
+
+  const child = spawn('ls', ['--file-type'], {cwd: path});
+
+  child.on('exit', (code, signal) => {
+    if(code !== 0)
+      console.log('process exited with code ', code, signal);
+  });
+
+  child.stdout.on('data', data => {
+    writeToFile(data);
+  });
+
+  child.stderr.on('data', data => {
+    console.error(`child stderr:\n${data}`);
+  });
+};
+
+listDirectory('/home/kangoula/Downloads');
