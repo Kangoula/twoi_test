d65055e23de5c2a262b041038bbcb66af93e93bb
Guillaume Denis <denisgme@gmail.com>
2018-09-03 15:34:26 +0200

    sort and group data by their keys

--- exercice_3.js
+++ exercice_3.js
@@ -30,11 +30,43 @@ let data = {
   '0-1_1-0': {car: {color:'blue'}},
   '0-3_1-1_0-0_0-0_0-0': {car: {type:'sport'}},
   '0-1_1-0_0-0': {car: {brand:'ferrari'}}
-}
-// expected output 
-let newData = [ 
+};
+// expected output
+let newData = [
   {label: 'face', emotion: {anger:0.1,happiness:0.9}, gender: 'female'},
   {label: 'car', car: {color:'blue', brand:'ferrari'}},
   {label: 'face', emotion: {anger:0.1,happiness:0.9}, gender: 'male'},
   {label: 'car', car: {color:'red', brand:'mazda', size:'big', type:'sport'}}
-]
+];
+
+/**
+ * Group objects from an array by their keys with a predicate function
+ *
+ * @param {Array} coll the array of objets we want to group
+ * @param {Function} fn the predicate function to decide if an object should be added in a group
+ *
+ * @returns {Object} an object with its keys set as the result of the group predicate and its values set as the grouped objects
+ */
+const groupByKey = (coll, fn) => {
+    let groupedMap = coll.reduce((acc, cur) => {
+        // apply the predicate function
+        const key = fn(cur),
+              elem = acc[key];
+        if(!elem) {
+            // create a new group
+            acc[key] = [cur];
+        } else {
+            // add elem in the group
+            elem.push(cur);
+        }
+        return acc;
+    }, {});
+
+    return groupedMap;
+};
+
+
+
+let sorted = Object.entries(data).sort();
+sorted = groupByKey(sorted, (e) => { return e[0].substring(0,3);});
+console.log("sorted", sorted);
